30ac268872dcb65962fe38a671dc3add9a1ddea2
aloks97 <alok@lanterne.ai>
2021-05-09 15:24:42 +0100

    Refactor processing of date and time columns

--- src/data_loaders/astro_seek_data_loader/processed_astro_seek_data_loader.py
+++ src/data_loaders/astro_seek_data_loader/processed_astro_seek_data_loader.py
@@ -20,25 +20,25 @@ class ProcessedAstroSeekDataLoader:
             (~self._raw_astro_seek_data["Birth latitude"].str.contains(","))
         ]
 
-        cleaned_astro_seek_data["Birth time - local"] = pd.to_datetime(cleaned_astro_seek_data["Birth time - local"], errors='coerce')
-        cleaned_astro_seek_data["Birth time - GMT"] = pd.to_datetime(cleaned_astro_seek_data["Birth time - GMT"], errors='coerce')
+        cleaned_astro_seek_data.dropna(inplace=True)
 
         return cleaned_astro_seek_data
 
     def _process_cleaned_astro_seek_data(self, cleaned_astro_seek_data):
-        cleaned_astro_seek_data["birth_date"] = cleaned_astro_seek_data["Birth time - GMT"].dt.date
-        cleaned_astro_seek_data["birth_time"] = cleaned_astro_seek_data["Birth time - GMT"].dt.time
-        cleaned_astro_seek_data["timezone"] = np.zeros(len(cleaned_astro_seek_data)).astype(int)
+        cleaned_astro_seek_data["birth_day"] = cleaned_astro_seek_data["Birth time - GMT"].apply(lambda x: x.split(' ')[0])
+        cleaned_astro_seek_data["birth_month"] = cleaned_astro_seek_data["Birth time - GMT"].apply(lambda x: x.split(' ')[1])
+        cleaned_astro_seek_data["birth_year"] = cleaned_astro_seek_data["Birth time - GMT"].apply(lambda x: x.split(' ')[2])
 
-        cleaned_astro_seek_data.occupation = cleaned_astro_seek_data.occupation.map(lambda x: x[:-1])
-        cleaned_astro_seek_data.rename(
-            {
-                "Birth longitude": "longitude",
-                "Birth latitude": "latitude"
-            }, inplace=True, axis="columns"
-        )
-
+        cleaned_astro_seek_data["birth_hour"] = cleaned_astro_seek_data["Birth time - GMT"].apply(lambda x: x.split('-')[-1].split(':')[0])
+        cleaned_astro_seek_data["birth_minute"] = cleaned_astro_seek_data["Birth time - GMT"].apply(lambda x: x.split('-')[-1].split(':')[1])
         cleaned_astro_seek_data.drop(["Birth time - local", "Birth time - GMT"], axis=1, inplace=True)
+
+        cleaned_astro_seek_data["timezone"] = np.zeros(len(cleaned_astro_seek_data)).astype(int)
+        cleaned_astro_seek_data.occupation = cleaned_astro_seek_data.occupation.map(lambda x: x[:-1])
+        rename_map = {
+            old_col_name: old_col_name.lower().replace(" ", "_") for old_col_name in cleaned_astro_seek_data.columns
+        }
+        cleaned_astro_seek_data.rename(rename_map, inplace=True, axis="columns")
 
         return cleaned_astro_seek_data
 
@@ -46,6 +46,8 @@ if __name__ == "__main__":
     raw_astro_seek_data = pd.read_csv("raw_astro_seek_chart_data.csv")
     processed_astro_seek_data_loader = ProcessedAstroSeekDataLoader(raw_astro_seek_data)
     processed_astro_seek_data = processed_astro_seek_data_loader.load_processed_astro_seek_data()
+    processed_astro_seek_data.to_csv("processed_astro_seek_data.csv")
 
+    print(processed_astro_seek_data.columns)
     print(processed_astro_seek_data.head())
     print(processed_astro_seek_data.isnull().sum(axis=0))
